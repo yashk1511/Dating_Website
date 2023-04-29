9c0c07a94f96a0e3e0e06c0e8c4ce192d9ca7408
Archisman Mukherjee <119628042+Archisman02@users.noreply.github.com>
2023-04-30 00:23:38 +0530

    Linked the MongoDB and setup user schema

--- BackEnd/controllers/user_controller.js
+++ BackEnd/controllers/user_controller.js
@@ -1,3 +1,3 @@
-module.exports.create = function(req, res){
-    return res.end('<h1>Controller is working.</h1>');
+module.exports.profile = function(req, res){
+    return res.end('<h1>User Profile</h1>');
 }
