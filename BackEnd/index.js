8ea1f14c5bb00cfb038f53a7c3d5fc82fbae2a50
Archisman Mukherjee <119628042+Archisman02@users.noreply.github.com>
2023-04-30 00:23:38 +0530

    Linked the MongoDB and setup user schema

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const db = require('./config/mongoose');
 
 // use express router
 app.use('/', require('./routes'));
+
+// set up the view engine
+// app.set('view engine', 'ejs');
+// app.set('views', require('./views'));
 
 app.listen(port, function(err){
     if(err){
